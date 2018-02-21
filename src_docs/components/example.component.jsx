fc2fd0a42497f0af6fa03616817eb32046e49055
bo-kh <boris.horosh@opuscapita.com>
2018-02-21 14:58:09 +0200

    Implemented flexible example container

--- src_docs/components/example.component.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Example from '../../src/index';
-
-export default function ComponentView() {
-  return (
-    <Example />
-  );
-}
--- src_docs/containers/example.container.jsx
+++ src_docs/containers/example.container.jsx
@@ -3,27 +3,21 @@ import { Panel, Grid, Row, Col } from 'react-bootstrap';
 import ExampleView from '../components/example-view.component';
 import GithubLogo from '../images/logo-github.svg';
 
+import './example.scss';
+
 export default () => (
-  <Grid>
-    <Row>
-      <Col xs={11} md={8} lg={6}>
-        <Row>
-          <Col xs={10}>
-            <h3>React component template</h3>
-          </Col>
-          <Col xs={2}>
-            <a
-              href="https://github.com/OpusCapita/react-component-template"
-              style={{ marginTop: '20px', display: 'block' }}
-            >
-              <GithubLogo />
-            </a>
-          </Col>
-        </Row>
-        <Panel>
-          <ExampleView />
-        </Panel>
-      </Col>
-    </Row>
-  </Grid>
+  <div className="example-container oc-flex-column">
+    <div className="example-header oc-flex-row">
+      <h3>React Hierarchy Selector</h3>
+      <a
+        className="example-git-logo"
+        href="https://github.com/OpusCapita/react-hierarchy-selector"
+      >
+        <span><GithubLogo /></span>
+      </a>
+    </div>
+    <div className="example-content oc-flex-column">
+      <ExampleView />
+    </div>
+  </div>
 );
